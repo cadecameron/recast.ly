4bf04fe482688526edf4be649dc9f58bbd1eed8f
cadecameron <cade.cameron@gmail.com>
2019-12-30 16:01:01 -0800

    Finish up to YouTube API

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,9 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = ({videos}) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {videos.map((video, i) => <VideoListEntry key={i} video={video}/> )}
+
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+import searchYouTube from './lib/searchYouTube.js';
+
+ReactDOM.render(<App /* VIDEOS HERE */ />,
+  document.getElementById('app')
+);
 
